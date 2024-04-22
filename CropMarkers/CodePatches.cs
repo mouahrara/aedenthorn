c2e54fd2972a5478e20ac4d52d72866d2688c35d
mouahrara <mouahrara@users.noreply.github.com>
2024-04-22 20:24:03 +0200

    Crop Markers 0.2.1-unofficial.2-mouahrara

--- CropMarkers/CodePatches.cs
+++ CropMarkers/CodePatches.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
+using StardewValley.GameData.Objects;
 using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 namespace CropMarkers
@@ -19,11 +20,16 @@ namespace CropMarkers
 				Vector2 pos = Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64 + Config.OffsetX, tileLocation.Y * 64 + Config.OffsetY - 4));
 				float base_sort = (float)(tileLocation.Y * 64 + Config.OffsetY) / 10000f + tileLocation.X / 50000f;
 
-				b.Draw(Game1.bigCraftableSpriteSheet, new Rectangle((int)(pos.X - 8 * size), (int)(pos.Y - 16 * size), (int)(16 * size), (int)(36 * size)), new Rectangle(80, 128, 16, 32), Color.White * opacity, 0, Vector2.Zero, SpriteEffects.None, base_sort - 1.2E-05f);
-				b.Draw(Game1.objectSpriteSheet, pos, new Rectangle?(Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, int.Parse(__instance.indexOfHarvest.Value), 16, 16)), Color.White * opacity, 0f, new Vector2(8f, 8f), size, SpriteEffects.None, base_sort - 1.1E-05f);
-				if (__instance.programColored.Value)
+				if (Game1.objectData.TryGetValue(__instance.indexOfHarvest.Value, out ObjectData harvestData))
 				{
-					b.Draw(Game1.objectSpriteSheet, pos, new Rectangle?(Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, int.Parse(__instance.indexOfHarvest.Value) + 1, 16, 16)), __instance.tintColor.Value * opacity, 0f, new Vector2(8f, 8f), size, SpriteEffects.None, base_sort - 1E-05f);
+					Texture2D harvestSpriteSheet = harvestData.Texture is null ? Game1.objectSpriteSheet : Game1.content.Load<Texture2D>(harvestData.Texture);
+
+					b.Draw(Game1.bigCraftableSpriteSheet, new Rectangle((int)(pos.X - 8 * size), (int)(pos.Y - 16 * size), (int)(16 * size), (int)(36 * size)), new Rectangle(80, 128, 16, 32), Color.White * opacity, 0, Vector2.Zero, SpriteEffects.None, base_sort - 1.2E-05f);
+					b.Draw(harvestSpriteSheet, pos, new Rectangle?(Game1.getSourceRectForStandardTileSheet(harvestSpriteSheet, harvestData.SpriteIndex, 16, 16)), Color.White * opacity, 0f, new Vector2(8f, 8f), size, SpriteEffects.None, base_sort - 1.1E-05f);
+					if (__instance.programColored.Value)
+					{
+						b.Draw(harvestSpriteSheet, pos, new Rectangle?(Game1.getSourceRectForStandardTileSheet(harvestSpriteSheet, harvestData.SpriteIndex + 1, 16, 16)), __instance.tintColor.Value * opacity, 0f, new Vector2(8f, 8f), size, SpriteEffects.None, base_sort - 1E-05f);
+					}
 				}
 			}
 		}
--- CropMarkers/release-notes.md
+++ CropMarkers/release-notes.md
@@ -2,6 +2,10 @@
 
 # Release notes
 
+## 0.2.1-unofficial.2-mouahrara
+Released on 22 April, 2024, for SMAPI 4.0.0 or later.
+* Added compatibility with modded crops and the new crops introduced in version 1.6.0 ✨
+
 ## 0.2.1-unofficial.1-mouahrara
 Released on 11 April, 2024, for SMAPI 4.0.0 or later.
 * Migrated to 1.6.0 and project cleanup 🚀
