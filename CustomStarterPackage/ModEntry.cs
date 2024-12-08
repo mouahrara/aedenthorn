565dbdccc56e19996a2ab93fb5a76ec49abb4d06
mouahrara <mouahrara@users.noreply.github.com>
2024-12-08 18:12:21 +0100

    Custom Starter Package 0.2.1-unofficial.3-mouahrara

--- CustomStarterPackage/ModEntry.cs
+++ CustomStarterPackage/ModEntry.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using HarmonyLib;
-using Microsoft.Xna.Framework;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
-using StardewValley.Inventories;
 using StardewValley.Objects;
-using StardewValley.Tools;
-using Object = StardewValley.Object;
 
 namespace CustomStarterPackage
 {
@@ -17,13 +13,13 @@ namespace CustomStarterPackage
 	{
 		internal static IMonitor SMonitor;
 		internal static IModHelper SHelper;
+		internal static IManifest SModManifest;
 		internal static ModConfig Config;
-
 		internal static ModEntry context;
 
-		public const string dictPath = "aedenthorn.CustomStarterPackage/dictionary";
+		public const string dictionaryPath = "aedenthorn.CustomStarterPackage/dictionary";
 		public const string chestKey = "aedenthorn.CustomStarterPackage/chest";
-		internal static Dictionary<string, StarterItemData> dataDict = new();
+		internal static Dictionary<string, StarterItemData> dataDictionary = new();
 
 		/// <summary>The mod entry point, called after the mod is first loaded.</summary>
 		/// <param name="helper">Provides simplified APIs for writing mods.</param>
@@ -32,9 +28,9 @@ namespace CustomStarterPackage
 			Config = Helper.ReadConfig<ModConfig>();
 
 			context = this;
-
 			SMonitor = Monitor;
 			SHelper = helper;
+			SModManifest = ModManifest;
 
 			helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
 			helper.Events.GameLoop.DayStarted += GameLoop_DayStarted;
@@ -57,208 +53,52 @@ namespace CustomStarterPackage
 			}
 		}
 
-		private void GameLoop_DayStarted(object sender, StardewModdingAPI.Events.DayStartedEventArgs e)
+		private void GameLoop_DayStarted(object sender, DayStartedEventArgs e)
 		{
 			if (!Config.ModEnabled)
 				return;
 
-			dataDict = Game1.content.Load<Dictionary<string, StarterItemData>>(dictPath);
-			SMonitor.Log($"Loaded {dataDict.Count} items from content patcher");
-			foreach (var o in Game1.player.currentLocation.objects.Pairs)
-			{
-				if (o.Value is Chest && (o.Value as Chest).giftbox.Value && (o.Value as Chest).Items.Count == 1 && (((o.Value as Chest).Items[0].QualifiedItemId.Equals("(O)472") && (o.Value as Chest).Items[0].Stack == 15) || ((o.Value as Chest).Items[0].QualifiedItemId.Equals("(O)178") && (o.Value as Chest).Items[0].Stack == 15)))
-				{
-					SMonitor.Log($"Found starter chest at {o.Key}; replacing");
-					Inventory items = new();
-
-					foreach (var d in dataDict)
-					{
-						if (d.Value.FarmTypes is not null)
-						{
-							if (d.Value.FarmTypes.All(farmType => farmType.StartsWith('!')))
-							{
-								if (d.Value.FarmTypes.Any(farmType => farmType[1..].Equals(Game1.GetFarmTypeKey())))
-									continue;
-							}
-							else
-							{
-								if (!d.Value.FarmTypes.Contains(Game1.GetFarmTypeKey()))
-									continue;
-							}
-						}
-						if (d.Value.ChancePercent < Game1.random.Next(100))
-							continue;
-						SMonitor.Log($"Adding {d.Key}");
-
-						Item obj;
-						string itemId;
-
-						switch (d.Value.Type)
-						{
-							case "Object":
-								int amount = (d.Value.MinAmount < d.Value.MaxAmount) ? Game1.random.Next(d.Value.MinAmount, d.Value.MaxAmount + 1) : d.Value.MinAmount;
-								int quality = (d.Value.MinQuality < d.Value.MaxQuality) ? Game1.random.Next(d.Value.MinQuality, d.Value.MaxQuality + 1) : d.Value.MinQuality;
-
-								if ((itemId = GetItemIdForNewDataModel(d.Value.Type, Game1.objectData, d.Value.NameOrIndex)) is null)
-									continue;
-								obj = new Object(itemId, amount, quality: quality);
-								break;
-							case "BigCraftable":
-							case "Chest":
-								if ((itemId = GetItemIdForNewDataModel(d.Value.Type, Game1.bigCraftableData, d.Value.NameOrIndex)) is null)
-									continue;
-								obj = new Object(Vector2.Zero, itemId, false);
-								break;
-							case "Hat":
-								if ((itemId = GetItemIdForOldDataModel(d.Value.Type, SHelper.GameContent.Load<Dictionary<string, string>>("Data/hats"), d.Value.NameOrIndex)) is null)
-									continue;
-								obj = new Hat(itemId);
-								break;
-							case "Boots":
-								if ((itemId = GetItemIdForOldDataModel(d.Value.Type, SHelper.GameContent.Load<Dictionary<string, string>>("Data/Boots"), d.Value.NameOrIndex)) is null)
-									continue;
-								obj = new Boots(itemId);
-								break;
-							case "Ring":
-								if ((itemId = GetItemIdForNewDataModel(d.Value.Type, Game1.objectData, d.Value.NameOrIndex)) is null)
-									continue;
-								obj = new Ring(itemId);
-								break;
-							case "Clothing":
-								if ((itemId = GetItemIdForNewDataModel(d.Value.Type, Game1.shirtData, d.Value.NameOrIndex, false)) is null && (itemId = GetItemIdForNewDataModel(d.Value.Type, Game1.pantsData, d.Value.NameOrIndex)) is null)
-									continue;
-								obj = new Clothing(itemId);
-								break;
-							case "Furniture":
-								if ((itemId = GetItemIdForOldDataModel(d.Value.Type, SHelper.GameContent.Load<Dictionary<string, string>>("Data/Furniture"), d.Value.NameOrIndex)) is null)
-									continue;
-								obj = Furniture.GetFurnitureInstance(itemId, Vector2.Zero);
-								break;
-							case "Tool":
-							case "Axe":
-							case "FishingRod":
-							case "Hoe":
-							case "Pan":
-							case "Pickaxe":
-							case "Shears":
-							case "WateringCan":
-								if ((itemId = GetItemIdForNewDataModel(d.Value.Type, Game1.toolData, d.Value.NameOrIndex)) is null)
-									continue;
-								obj = ItemRegistry.Create("(T)" + itemId, 1, 0, true);
-								if (obj is WateringCan)
-								{
-									(obj as WateringCan).WaterLeft = 0;
-								}
-								break;
-							case "MeleeWeapon":
-								if ((itemId = GetItemIdForNewDataModel(d.Value.Type, Game1.weaponData, d.Value.NameOrIndex)) is null)
-									continue;
-								obj = new MeleeWeapon(itemId);
-								break;
-							default:
-								SMonitor.Log($"Object type {d.Value.Type} not recognized", LogLevel.Warn);
-								continue;
-						}
-						if (obj != null)
-						{
-							items.Add(obj);
-						}
-						else
-						{
-							SMonitor.Log($"Object {d.Key} not recognized", LogLevel.Warn);
-						}
-					}
-					if (items.Count > 0)
-					{
-						(o.Value as Chest).Items.Clear();
-						(o.Value as Chest).Items.AddRange(items);
-						(o.Value as Chest).dropContents.Value = true;
-						o.Value.modData[chestKey] = "true";
-						SMonitor.Log($"Added {items.Count} items to starter package", LogLevel.Info);
-						return;
-					}
-					SMonitor.Log($"No items added to the starter package (The default starter package will be used)", LogLevel.Info);
-					return;
-				}
-			}
+			ReplaceDefaultStarterPackage();
 		}
 
-		private static string GetItemIdForOldDataModel(string type, Dictionary<string, string> data, string nameOrId, bool log = true)
+		private void Content_AssetRequested(object sender, AssetRequestedEventArgs e)
 		{
-			if (!data.TryGetValue(nameOrId, out _))
+			if (e.NameWithoutLocale.IsEquivalentTo(dictionaryPath))
 			{
-				try
-				{
-					return data.First(o => o.Value.StartsWith(nameOrId + '/')).Key;
-				}
-				catch
-				{
-					if (log)
-					{
-						SMonitor.Log($"{type} {nameOrId} not found", LogLevel.Warn);
-					}
-					return null;
-				}
-			}
-			return nameOrId;
-		}
-
-		private static string GetItemIdForNewDataModel<T>(string type, IDictionary<string, T> data, string nameOrId, bool log = true)
-		{
-			if (!data.TryGetValue(nameOrId, out _))
-			{
-				try
-				{
-					return data.First(o => typeof(T).GetField("Name").GetValue(o.Value).Equals(nameOrId)).Key;
-				}
-				catch
-				{
-					if (log)
-					{
-						SMonitor.Log($"{type} {nameOrId} not found", LogLevel.Warn);
-					}
-					return null;
-				}
-			}
-			return nameOrId;
-		}
-
-		private void Content_AssetRequested(object sender, StardewModdingAPI.Events.AssetRequestedEventArgs e)
-		{
-			if (e.NameWithoutLocale.IsEquivalentTo(dictPath))
-			{
-				e.LoadFrom(() => new Dictionary<string, StarterItemData>(), StardewModdingAPI.Events.AssetLoadPriority.Exclusive);
+				e.LoadFrom(() => new Dictionary<string, StarterItemData>(), AssetLoadPriority.Exclusive);
 			}
 		}
 
-		private void GameLoop_SaveLoaded(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e)
+		private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs e)
 		{
 			if (!Config.ModEnabled)
 				return;
 
-			dataDict = Game1.content.Load<Dictionary<string, StarterItemData>>(dictPath);
+			dataDictionary = Game1.content.Load<Dictionary<string, StarterItemData>>(dictionaryPath);
 		}
 
-		private void GameLoop_GameLaunched(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
+		private void GameLoop_GameLaunched(object sender, GameLaunchedEventArgs e)
 		{
-			// get Generic Mod Config Menu's API (if it's installed)
-			var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
-			if (configMenu is null)
-				return;
+			// Get Generic Mod Config Menu's API
+			IGenericModConfigMenuApi gmcm = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
 
-			// register mod
-			configMenu.Register(
-				mod: ModManifest,
-				reset: () => Config = new ModConfig(),
-				save: () => Helper.WriteConfig(Config)
-			);
+			if (gmcm is not null)
+			{
+				// Register mod
+				gmcm.Register(
+					mod: ModManifest,
+					reset: () => Config = new ModConfig(),
+					save: () => Helper.WriteConfig(Config)
+				);
 
-			configMenu.AddBoolOption(
-				mod: ModManifest,
-				name: () => SHelper.Translation.Get("GMCM.ModEnabled.Name"),
-				getValue: () => Config.ModEnabled,
-				setValue: value => Config.ModEnabled = value
-			);
+				// Main section
+				gmcm.AddBoolOption(
+					mod: ModManifest,
+					name: () => SHelper.Translation.Get("GMCM.ModEnabled.Name"),
+					getValue: () => Config.ModEnabled,
+					setValue: value => Config.ModEnabled = value
+				);
+			}
 		}
 	}
 }
--- CustomStarterPackage/release-notes.md
+++ CustomStarterPackage/release-notes.md
@@ -2,10 +2,28 @@
 
 # Release notes
 
+## 0.2.1-unofficial.3-mouahrara
+Released on 9 December, 2024, for SMAPI 4.1.0 or later.
+* Improved support for custom farms ✨
+
+> [!TIP]
+> The farm type of a custom farm corresponds to its **Id** in the asset `Data/AdditionalFarms`. To get the farm type of a custom farm added via **Content Patcher**, you can run the SMAPI console command `patch export "Data/AdditionalFarms"`, then check the **Id** of the farm in the file `{GamePath}/patch export/Data_AdditionalFarms.json`.
+
 ## 0.2.1-unofficial.2-mouahrara
 Released on 12 May, 2024, for SMAPI 4.0.0 or later.
 * Added the **FarmTypes** field to specify a list of farms where the item should be included in the starter package. If all types of farms in the list are preceded by `!`, then the list is considered as an exclusion list, and the item is added to the starter package of all farms except those in the list ✨
 * Fixed an issue where the mod was not working on Meadowlands Farm 🔧
+
+> [!NOTE]
+> The following are the farm types for the base game farms:
+> * Standard Farm: `Standard`
+> * Riverland Farm: `Riverland`
+> * Forest Farm: `Forest`
+> * Hill-top Farm: `Hilltop`
+> * Wilderness Farm: `Wilderness`
+> * Four Corners Farm: `FourCorners`
+> * Beach Farm: `Beach`
+> * Meadowlands Farm: `MeadowlandsFarm`
 
 ## 0.2.1-unofficial.1-mouahrara
 Released on 1 May, 2024, for SMAPI 4.0.0 or later.
